db9257139b5b6e672702543050c7b8bb150de986
Tamas K. Papp <tkpapp@gmail.com>
2018-01-10 11:05:27 +0100

    Initial version.

--- src/HighestDensityRegions.jl
+++ src/HighestDensityRegions.jl
@@ -1,5 +1,110 @@
+__precompile__()
 module HighestDensityRegions
 
-# package code goes here
+using ArgCheck
+using DocStringExtensions
+
+export hdr_thresholds
+
+
+# utilities
+
+"""
+    $SIGNATURES
+
+Test if the given numbers are valid probabilities.
+"""
+is_valid_probabilities(αs) =  all(0 .≤ αs .≤ 1)
+
+"""
+    $SIGNATURES
+
+When `i == 0`, return `0` (with the element type of the vector), otherwise
+behaves like `getindex`.
+"""
+@inline getindex_or_zero(x::AbstractVector{T}, i) where T =
+    i == 0 ? zero(T) : x[i]
+
+"""
+    $SIGNATURES
+
+Find `i` in the sorted vector `xs` such that `xs[i-1] < α ≤ xs[i]`, with the
+assumed extension ``xs[0] = 0``.
+
+Return `γ, i` such that `interpolate_in(xs, γ, i) ≈ α`. See
+[`interpolate_in`](@ref).
+"""
+function interpolation_coefficient_and_index(xs::Vector{T}, α) where {T <: Real}
+    i = searchsortedfirst(xs, α)
+    right = xs[i]
+    left = getindex_or_zero(xs, i - 1)
+    γ = (α - left) / (right - left)
+    γ, i
+end
+
+"""
+    $SIGNATURES
+
+Calculate the linear interpolation `(1-γ)*xs[i-1] + γ*xs[i]`, with the assumed
+extension ``xs[0] = 0``.
+"""
+function interpolate_in(xs, γ, i)
+    left = getindex_or_zero(xs, i - 1)
+    right = xs[i]
+    (1 - γ) * left + γ * right
+end
+
+"""
+    $SIGNATURES
+
+Return the thresholds for highest density regions with probability `αs`, given
+the sorted densities and cumulative probabilities (using the same sorting order)
+for each bin.
+"""
+function interpolated_density_thresholds(αs, sorted_densities, cumprobs)
+    @argcheck is_valid_probabilities(αs)
+    map(αs) do α
+        γ, i = interpolation_coefficient_and_index(cumprobs, 1 - α)
+        interpolate_in(sorted_densities, γ, i)
+    end
+end
+
+"""
+    $SIGNATURES
+
+Cumulative probability.
+"""
+cumprob(x) = cumsum(x) |> x -> x ./ x[end]
+
+
+# interface
+
+"""
+    $SIGNATURES
+
+Return thresholds for the highest density regions with coverage probabilities
+`αs`.
+
+`counts` is a vector proportional to count data for some bin structure, while
+`densities` is a vector of corresponding densities. For each bin, it is assumed
+that `counts[i] = densities[i] * bin_size[i]`, where `bin_size[i]` is the
+measure of the bin.
+
+A highest density region threshold `t` for a given `α` satisfies
+`sum(counts[densities .> t]) ≈ \alpha * sum(counts)`.
+
+When bins have the same size, `counts` may be omitted and assumed to be
+proportional to `densities`.
+"""
+function hdr_thresholds(αs, densities::AbstractVector, counts::AbstractVector)
+    order = sortperm(densities)
+    sorted_densities = densities[order]
+    interpolated_density_thresholds(αs, sorted_densities, cumprob(counts[order]))
+end
+
+function hdr_thresholds(αs, densities)
+    sorted_densities = sort(densities)
+    interpolated_density_thresholds(αs, sorted_densities, cumprob(sorted_densities))
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,88 @@
 using HighestDensityRegions
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+using HighestDensityRegions:
+    getindex_or_zero, interpolation_coefficient_and_index, interpolate_in
+
+using Distributions
+using DocStringExtensions
+
+@testset "utilities" begin
+    # getindex
+    v = 1:3
+    @test getindex_or_zero(v, 3) ≡ 3
+    @test getindex_or_zero(v, 0) ≡ 0
+
+    # interpolation index: arbitrary cumprobs
+    c = [0.1, 0.4, 1.0]
+    @test interpolation_coefficient_and_index(c, 0.1) == (1.0, 1)
+    @test interpolation_coefficient_and_index(c, 0.2) == (1/3, 2)
+    for _ in 1:20
+        α = rand()
+        γ, i = interpolation_coefficient_and_index(c, α)
+        @test interpolate_in(c, γ, i) ≈ α
+    end
+    for (i, α) in enumerate(c)
+        @test interpolation_coefficient_and_index(c, α) == (1.0, i)
+    end
+
+    # interpolation index: integer cumprobs
+    N = 8
+    cN = Vector((1:N) ./ N)
+    for (i, α) in enumerate(cN)
+        @test interpolation_coefficient_and_index(cN, α) == (1.0, i)
+    end
+end
+
+"""
+    $SIGNATURES
+
+Calculate densities and counts using the cumulative density function for a
+univariate distribution.
+"""
+function analytical_densities_and_counts(distribution, breaks)
+    @assert issorted(breaks)
+    counts = diff(cdf.(distribution, breaks))
+    densities = counts ./ diff(breaks)
+    densities, counts
+end
+
+"""
+    $SIGNATURES
+
+Calculate the empirical coverage probability of a (HDR) density threshold.
+"""
+coverage(threshold, densities, counts) =
+    sum(counts[densities .≥ threshold]) / sum(counts)
+
+"""
+    $SIGNATURES
+
+Test empirical coverage of HDR thresholds. `atol` is used for comparison, use
+dense breaks for more precision.
+"""
+function test_coverage(αs, thresholds, densities, counts, atol = 0.01)
+    for (i, α) in enumerate(αs)
+        @test coverage(thresholds[i], densities, counts) ≈ α atol = atol
+    end
+end
+
+@testset "normal thresholds with evenly spaced breaks" begin
+    N = 1000
+    densities, counts = analytical_densities_and_counts(Normal(0, 1),
+                                                        linspace(-4, 4, N))
+    αs = [0.25, 0.5, 0.75, 0.9]
+    thresholds = hdr_thresholds(αs, densities, counts)
+    test_coverage(αs, thresholds, densities, counts)
+    thresholds_even = hdr_thresholds(αs, densities)
+    @test thresholds_even ≈ thresholds
+end
+
+@testset "gamma thresholds with log breaks" begin
+    breaks = exp.(linspace(0, 2, 1000)) .- 1
+    densities, counts = analytical_densities_and_counts(Gamma(1, 1),
+                                                        linspace(-4, 4, 1000))
+    αs = [0.25, 0.5, 0.75, 0.9]
+    thresholds = hdr_thresholds(αs, densities, counts)
+    test_coverage(αs, thresholds, densities, counts)
+end
