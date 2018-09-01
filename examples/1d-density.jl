2e14f9949a252e60b112e4e2772fc7b1c520cfb9
Tamas K. Papp <tkpapp@gmail.com>
2018-09-01 09:56:47 +0200

    thicker plot

--- examples/1d-density.jl
+++ examples/1d-density.jl
@@ -11,6 +11,6 @@ ygrid = pdf.(Ref(k), xgrid)
 threshold = first(hdr_thresholds([0.5], ygrid))
 
 p = @pgf Axis({ xlabel = "x", ylabel = "kernel density", width = "10cm", height = "8cm" },
-              Plot({ thick, mesh, point_meta = "explicit" , shader = "interp"},
+              Plot({ very_thick, mesh, point_meta = "explicit" , shader = "interp"},
                    Coordinates(xgrid, ygrid; meta = Int.(threshold .< ygrid))))
 pgfsave("1d-density.svg", p)
